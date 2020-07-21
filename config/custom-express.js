d5ccbf19ba5e9815e3c6dd518f10a73d8533d6b9
Felipe <lfelp@msn.com>
2020-07-21 14:29:44 -0300

    environment changes

--- config/custom-express.js
+++ config/custom-express.js
@@ -5,7 +5,11 @@ const bodyParser = require('body-parser');
 
 module.exports = () => {
     const app = express();
-
+    app.use(bodyParser.json());
+    
+    consign()
+        .include('controllers')
+        .into(app)
     return app;
 
 }
