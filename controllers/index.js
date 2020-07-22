27a3888ef3819a7ae082907b6e81767da70b26e4
Felipe <lfelp@msn.com>
2020-07-22 18:30:10 -0300

    correção da view

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,34 @@
 module.exports = app =>{
 
     app.get('/', (req,res) => {
-        res.sendFile('/app/views/index.html');
+        res.send(`<table >
+        <thead >
+            <tr >
+                <th >Link</th><th >Desciçao</th>
+            </tr>
+        </thead>
+        <tr >
+            <td><a href="/games/list">Listar Partidas</a></td>
+            <td>Exibe um resumo da partida com o frag de cada player</td>
+        </tr>
+        <tr >
+            <td><a href="/games/kills">Listar Causas de Morte</a></td>
+            <td>Exibe de a quantidade de mortes agrupadas por causa</td>
+        </tr>
+        <tr >
+            <td>
+                <input type="text" placeholder="id do jogo"  id="id">
+                
+            </td>
+            <td>
+                <button  onclick="redirect()">Buscar partida por id</button>
+            </td>
+        </tr>
+    </table><script>
+    function redirect(){
+        document.location.href = '/games/list/' +document.getElementById('id').value;
+    }
+    </script>
+    `);
     })
 }
